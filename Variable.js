705d5b8f05ed0743897fea112745b38774e1ec53
Maher Jeevan <jeevanmaher5408@gmail.com>
2024-10-15 23:18:41 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,8 @@
+const AgeLimit= 18
+let StudentName = "Jeevan"
+var studentEmail = "jeevan@gmail.com"
+ var mobile;
+
+StudentName = "Maher"
+studentEmail = "maher@gmail.com"
+console.table([AgeLimit, StudentName,studentEmail, mobile])
